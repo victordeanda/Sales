e0139569fc6634b2ecd45190df6b0df13c89414d
victordeanda <victordeanda_3@hotmail.com>
2018-12-20 01:09:26 -0800

    Video 4
    Todo lo que hizo hasta el video 4
    Crear Data Context
    Modelo Producto
    Controlador Producto

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public Decimal Price { get; set; }
+
+        public bool IsAvailable{ get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Sales.Domain.Models
+{
+    public class DataContext :  DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+                
+        }
+    }
+}
